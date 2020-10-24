f0ec30a9f7966e71fdce997f6cf73f1c4fce2c62
david barkhuizen <david.barkhuizen@gmail.com>
2020-10-24 14:15:32 +0200

    trivial parse google attestaion key chain der

--- node/dist/device.controller.js
+++ node/dist/device.controller.js
@@ -26,14 +26,13 @@ exports.router = void 0;
 const express_1 = __importDefault(require("express"));
 const forge = __importStar(require("node-forge"));
 exports.router = express_1.default.Router();
-exports.router.post('/register', function (req, res) {
-    console.log('registering device...');
-    console.log(req.body.asn1hex);
-    // ---------------------------------------------------------------
-    var certAsn1 = forge.asn1.fromDer(Buffer.from(req.body.asn1hex, 'hex').toString('binary'));
+const describeCert = (label, hex) => {
+    var _a, _b, _c, _d;
+    var certAsn1 = forge.asn1.fromDer(Buffer.from(hex, 'hex').toString('binary'));
     var cert = forge.pki.certificateFromAsn1(certAsn1);
-    const issuerCN = cert.issuer.getField('CN').value;
-    const subjectCN = cert.subject.getField('CN').value;
+    const issuerCN = (_b = (_a = cert.issuer.getField('CN')) === null || _a === void 0 ? void 0 : _a.value) !== null && _b !== void 0 ? _b : 'no issuer';
+    const subjectCN = (_d = (_c = cert.subject.getField('CN')) === null || _c === void 0 ? void 0 : _c.value) !== null && _d !== void 0 ? _d : 'no subject';
+    console.log(`CERT: ${label}`);
     const description = [
         `issuer ${issuerCN}`,
         `subject ${subjectCN}`,
@@ -41,7 +40,21 @@ exports.router.post('/register', function (req, res) {
         `valid: ${cert.validity.notBefore} - ${cert.validity.notAfter}`
     ];
     console.log(description.join('\n'));
-    console.log(cert.subject.attributes);
-    res.send('device registration incomplete - not yet implemented');
+};
+exports.router.post('/register', function (req, res) {
+    console.log('registering device...');
+    console.log('device public key');
+    console.log(req.body.asn1hex);
+    console.log(req.body.attestationChain);
+    describeCert('PUBLIC KEY', req.body.asn1hex);
+    req.body.chain.forEach(function (value, i) {
+        console.log('%d: %s', i, value);
+    });
+    console.log('CHAIN:');
+    req.body.chain.forEach((cert, i) => {
+        describeCert(`LINK ${i}`, cert);
+    });
+    // ---------------------------------------------------------------
+    res.status(500).send('not yet implemented');
 });
 //# sourceMappingURL=device.controller.js.map
--- node/src/device.controller.ts
+++ node/src/device.controller.ts
@@ -3,19 +3,16 @@ import * as forge from 'node-forge';
 
 export const router = express.Router();
 
-router.post('/register', function (req, res) {
+const describeCert = (label: string, hex: string) => {
 
-    console.log('registering device...');
-    
-    console.log(req.body.asn1hex);
-
-    // ---------------------------------------------------------------
-
-    var certAsn1 = forge.asn1.fromDer(Buffer.from(req.body.asn1hex, 'hex').toString('binary'));
+    var certAsn1 = forge.asn1.fromDer(Buffer.from(hex, 'hex').toString('binary'));
     var cert = forge.pki.certificateFromAsn1(certAsn1);
 
-    const issuerCN = cert.issuer.getField('CN').value;
-    const subjectCN = cert.subject.getField('CN').value;
+    const issuerCN = cert.issuer.getField('CN')?.value ?? 'no issuer';
+    
+    const subjectCN = cert.subject.getField('CN')?.value ?? 'no subject';
+
+    console.log(`CERT: ${label}`);
 
     const description = [
         `issuer ${issuerCN}`, 
@@ -25,8 +22,28 @@ router.post('/register', function (req, res) {
     ];
     
     console.log(description.join('\n'));
+}
 
-    console.log(cert.subject.attributes);
+router.post('/register', function (req, res) {
 
-    res.send('device registration incomplete - not yet implemented');
+    console.log('registering device...');
+    
+    console.log('device public key');
+    console.log(req.body.asn1hex);
+    console.log(req.body.attestationChain);
+
+    describeCert('PUBLIC KEY', req.body.asn1hex);
+
+    req.body.chain.forEach(function (value, i) {
+        console.log('%d: %s', i, value);
+    });
+    
+    console.log('CHAIN:')
+    req.body.chain.forEach((cert, i) => {
+        describeCert(`LINK ${i}`, cert)
+    })
+
+    // ---------------------------------------------------------------
+
+    res.status(500).send('not yet implemented');
 });
